9daba83e8021165fc2a189c0f273813eec66c078
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-12-09 23:59:47 +0200

    option 1 for redirect

---
+++ src/views/OAuthPage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+
+    mounted() {
+        this.$http.get('get_token').then(() => {
+            console.log(localStorage)
+        })
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
